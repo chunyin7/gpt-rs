7c6382a213fed0b5560173b7ae27e08184684f8c
milesng <miles.c.y.ng@gmail.com>
2025-10-22 23:08:30 +1100

    embedding

--- src/model/embedding.rs
+++ src/model/embedding.rs
@@ -1,1 +1,30 @@
-pub struct Embedding {}
+use crate::matrix::matrix::Matrix;
+
+pub struct Embedding {
+    weights: Matrix,
+    dim: usize,
+    vocab_size: usize,
+}
+
+impl Embedding {
+    pub fn new(dim: usize, vocab_size: usize) -> Self {
+        Embedding {
+            weights: Matrix::new(vocab_size, dim),
+            dim,
+            vocab_size,
+        }
+    }
+
+    pub fn embed(&self, tokens: &[usize]) -> Result<Matrix, String> {
+        let mut ret = Matrix::new(tokens.len(), self.dim);
+
+        for (i, token) in tokens.iter().enumerate() {
+            let row = self.weights.row(*token)?;
+            for (j, val) in row.iter().enumerate() {
+                ret.set(i, j, *val)?;
+            }
+        }
+
+        Ok(ret)
+    }
+}
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,1 +1,2 @@
+use super::matrix;
 pub mod embedding;
